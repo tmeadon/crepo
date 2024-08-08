d0a829ef48e9924de73ef390add98d33cedbd772
Tom Meadon <tom@meadon.me>
2024-08-08 09:41:46 +0100

    add install script

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-# download binary to /usr/local/bin
+#!/bin/bash
 
+# Download binary to /usr/local/bin
+curl -L https://github.com/tmeadon/crepo/releases/download/v0.1.6/crepo.tar.gz -o /tmp/crepo.tar.gz
+tar -xzvf /tmp/crepo.tar.gz -C /usr/local/bin
 
-# print the wrapper function to .bashrc
+# Print the wrapper function to .bashrc
 /usr/local/bin/crepo --print >> ~/.bashrc
 
+# Source the updated .bashrc
 . ~/.bashrc
